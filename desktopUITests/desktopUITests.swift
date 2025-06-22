dfacf16133a26186ade2afac89ecf65d36d28844
LEE <15267030696@163.com>
2025-06-22 21:56:34 +0800

    1.新增 testCursor

--- desktopUITests/desktopUITests.swift
+++ desktopUITests/desktopUITests.swift
@@ -14,31 +14,31 @@ import XCTest
 
  */
 final class desktopUITests: XCTestCase {
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     @MainActor
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
-
+        
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
     
-    func showAppWindows(app: XCUIApplication){
+    
+    func showAppWindows(app: XCUIElement){
         
         // UI tests must launch the application that they test.
         // 遍历所有窗口并打印属性
@@ -54,6 +54,45 @@ final class desktopUITests: XCTestCase {
             print("窗口是否存在：\(window.exists)")
             print("窗口坐标：\(window.frame)")
             print("------------------------")
+        }
+    }
+    
+    func showAppMenu(app: XCUIElement){
+        
+        // UI tests must launch the application that they test.
+        // 遍历所有窗口并打印属性
+        for menuBar in app.menuBars.allElementsBoundByIndex {
+            // 打印窗口的调试描述（包含层级、标识符等信息）
+            print("窗口调试描述：\(menuBar.debugDescription)")
+            
+            // 也可单独获取部分属性，比如窗口标题（若有）
+            let title = menuBar.title
+            print("窗口标题：\(title)")
+            
+            // 还能获取其他属性，如是否存在、坐标等
+            print("窗口是否存在：\(menuBar.exists)")
+            print("窗口坐标：\(menuBar.frame)")
+            print("------------------------")
+        }
+    }
+    
+    // 递归打印菜单结构
+    func printMenuStructure(menu: XCUIElement, level: Int) {
+        let indent = String(repeating: "  ", count: level)
+        
+        // 打印当前菜单
+        let title = menu.title
+        print("\(indent)- \(title)")
+        
+        
+        // 遍历菜单项
+        for item in menu.menuItems.allElementsBoundByIndex {
+            print("\(indent)  - \(item.label)")
+            
+            // 如果菜单项有子菜单，递归打印
+            
+            printMenuStructure(menu: item, level: level + 1)
+            
         }
     }
     
@@ -1333,6 +1372,225 @@ final class desktopUITests: XCTestCase {
         
         
         targetApp.terminate()
+    }
+    
+    /**
+     Cursor Version: 1.1.2 VSCode Version: 1.96.2
+      1.需要有一个最近的工程，因为会默认点击第一个最近的工程  ⚠️⚠️⚠️
+     
+     */
+    func testCursor() throws {
+        let targetApp = XCUIApplication(bundleIdentifier: "com.todesktop.230313mzl4w4u92")
+        
+        // 1.启动Cursor
+        targetApp.launch()
+        
+        XCTAssertTrue(targetApp.wait(for: .runningForeground, timeout: 5), "Cursor 启动失败")
+        
+        
+        
+        let fileMenuBarItem = targetApp.menuBarItems["File"].firstMatch
+        fileMenuBarItem.click()
+        
+        let recentMenuItem = fileMenuBarItem.menuItems["Open Recent"].firstMatch
+        recentMenuItem.click()
+        
+        // 最近的一个工程进行点击
+        let thirdMenuItem = recentMenuItem.menuItems.allElementsBoundByAccessibilityElement[2]
+        thirdMenuItem.click()
+        
+        
+        
+        // 5.启动我们的 Glotera app
+        let app = XCUIApplication()
+        app.launch()
+        
+        
+        // 6.获取编辑框
+        
+        
+        
+   
+
+        var newChatButton = targetApp.buttons["New Chat (⌘N) [⌥] New Tab (⌘T)"]
+        var settingButton = targetApp.popUpButtons["Close, Export, Settings and More..."]
+
+        // 不存在则打开右侧边栏，使编辑框显示
+        if !newChatButton.waitForExistence(timeout: 10) {
+            let rightCheckBox = targetApp.checkBoxes["Toggle AI Pane (⌥⌘B)"]
+            rightCheckBox.click()
+            newChatButton = targetApp.buttons["New Chat (⌘N) [⌥] New Tab (⌘T)"]
+            settingButton = targetApp.popUpButtons["Close, Export, Settings and More..."]
+        }
+        
+        var predicate = NSPredicate(format: "value CONTAINS[c] 'Plan, search'")
+        settingButton.click()
+        var clearAllChatMenuItem = targetApp.menuItems["Clear All Chats"]
+        clearAllChatMenuItem.click()
+        
+        newChatButton.click()
+        var textEditor = targetApp.staticTexts.matching(predicate).firstMatch
+        
+        
+        var pos_begin = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 0.02, dy: 0.02))
+        var pos_end = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 1, dy: 1))
+        /** TEST1:          选中中间的"你好呀",然后弹窗进行翻译，翻译后替换原来中间的”你好呀“，并检测正确性   -----------------    **/
+        // TEST1.1.全选已有内容
+        
+        
+        let testString = "你好呀"
+        let blankString = ". "
+        let testStringLen = testString.count
+        let testStringWidth = 50
+        let Y = 0
+        textEditor.typeText(testString + blankString + testString + blankString + testString + blankString)
+        
+        // 重新获得焦点
+        targetApp.activate()
+        
+        // 验证应用是否已回到前台
+        XCTAssertTrue(targetApp.wait(for: .runningForeground, timeout: 5))
+        
+        predicate = NSPredicate(format: "value CONTAINS[c] %@",testString)
+        
+        textEditor = targetApp.staticTexts.matching(predicate).firstMatch
+        
+        
+        let pos0 = textEditor.coordinate(withNormalizedOffset:.zero)
+        let pos1 = textEditor.coordinate(withNormalizedOffset:.zero).withOffset(CGVector(dx: testStringWidth, dy: Y))
+        
+        let pos2 = textEditor.coordinate(withNormalizedOffset:.zero).withOffset(CGVector(dx: testStringWidth*2, dy:Y))
+        let pos3 = textEditor.coordinate(withNormalizedOffset:.zero).withOffset(CGVector(dx: testStringWidth*3, dy: Y))
+        
+        
+        let pos4 = textEditor.coordinate(withNormalizedOffset:.zero).withOffset(CGVector(dx: testStringWidth*4, dy: Y))
+        
+        // TEST1.2.选中部分文字进行弹窗翻译后插入，选中的是"好呀  你"，翻译后应该满足 "你好呀  你.+好呀  "
+        pos1.click(forDuration: 1, thenDragTo: pos2)
+        
+        var windowsQuery = app.windows
+        var popUpButton = app.popUpButtons.element(boundBy: 0)
+        popUpButton.click()
+        windowsQuery = app.windows
+        
+        // TEST1.3.选中英语进行翻译
+        windowsQuery.menuItems["English"].click()
+        
+ 
+        
+        // TEST1.3.自动插入后，检验结果是否满足 "你好呀  你.+好呀  " 正则表达式
+        var translatedString : String! = textEditor.value as! String
+        print("translatedString:" + translatedString)
+        // 定义正则表达式："你好呀  你.+好呀  "
+        let pattern = "你好呀.\\s+你.+好呀."
+        
+        
+        let regex = try NSRegularExpression(pattern: pattern)
+        let matches = regex.matches(in: translatedString, range: NSRange(translatedString.startIndex..., in: translatedString))
+        
+        
+        XCTAssertTrue(matches.count > 0, "输入框部分翻译插入失败")
+        
+        pos_begin = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 0.02, dy: 0.02))
+        pos_end = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 1, dy: 1))
+        
+        /** TEST2:          全选输入框文字,然后弹窗进行翻译，翻译后整体替换原来输入框文字，并检测正确性   -----------------    **/
+        // TEST2.1.全选已有内容
+        pos_begin.click(forDuration: 1, thenDragTo: pos_end)
+        
+        
+        textEditor.typeText(testString + blankString + testString + blankString + testString + blankString)
+        
+        
+        // TEST2.2.输入 #en 触发翻译
+        textEditor.typeText("#en ")
+        
+        
+      
+        
+        predicate = NSPredicate(format: "value CONTAINS[c] 'Hello there. Hello'")
+        
+        textEditor = targetApp.staticTexts.matching(predicate).firstMatch
+        
+        textEditor.waitForExistence(timeout: 20)
+        
+        // TEST2.3.整体翻译后，检查结果是否是 "Hello there, hello there, hello there."
+        translatedString = textEditor.value as! String
+        print("translatedString:" + translatedString)
+        
+        
+        // 定义正则表达式："你好呀  你.+好呀  "
+        var matchString = "Hello there. Hello there. Hello there."
+        
+        
+    //    XCTAssertTrue(translatedString == matchString, "输入框整体翻译插入失败")
+        
+    
+        textEditor.typeText(" 直接输出前面这3句英文，其他不要输出\n")
+        
+        pos_begin = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 0.02, dy: 0.02))
+        pos_end = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 1, dy: 0.02))
+
+        pos_begin.click()
+        textEditor = targetApp.staticTexts.matching(predicate).firstMatch
+        pos_begin = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 0.02, dy: 0.02))
+        pos_end = textEditor.coordinate(withNormalizedOffset: CGVector(dx: 1, dy: 0.02))
+        pos_begin.click(forDuration: 1, thenDragTo: pos_end)
+        textEditor.typeText(" ceshi")
+        
+
+        // 重新获得焦点
+        targetApp.activate()
+        
+        // 验证应用是否已回到前台
+        XCTAssertTrue(targetApp.wait(for: .runningForeground, timeout: 5))
+        
+        showAppWindows(app: targetApp)
+        
+        let lastMessageLabel : XCUIElement! = targetApp.staticTexts.matching(predicate).firstMatch
+        lastMessageLabel.waitForExistence(timeout: 20)
+        print("lastMessageLabel:\(lastMessageLabel.debugDescription)")
+        sleep(3)
+        let lastMessageLabelFrame = lastMessageLabel.frame
+        let endX = lastMessageLabelFrame.width
+        let midX = lastMessageLabelFrame.width - 150
+        let startX = endX - 210
+        let midY = lastMessageLabelFrame.height / 2.0
+        
+        // TEST3.2.选中
+        let lastMessageLabelMidPoint = lastMessageLabel.coordinate(withNormalizedOffset: .zero).withOffset(CGVector(dx:midX,dy: midY))
+        lastMessageLabel.coordinate(withNormalizedOffset: .zero).withOffset(CGVector(dx:endX,dy: midY)).click(forDuration: 1.0, thenDragTo: lastMessageLabel.coordinate(withNormalizedOffset: .zero).withOffset(CGVector(dx:startX,dy: midY)), withVelocity: 1000, thenHoldForDuration: 1)
+        
+        
+        popUpButton = app.popUpButtons.element(boundBy: 0)
+        print("app:\(app.debugDescription),app.popUpButtons:\(app.popUpButtons)")
+        popUpButton.click()
+        windowsQuery = app.windows
+        
+        // TEST3.3.选中中文进行翻译
+        windowsQuery.menuItems["中文"].click()
+        
+        sleep(3)
+        
+        
+        // TEST3.4.检测翻译结果
+        
+        let appScrollView = app.scrollViews.firstMatch
+        let translatedStaticText = appScrollView.staticTexts.firstMatch
+        translatedString = translatedStaticText.value as! String
+        print("translatedString:" + translatedString)
+        
+        
+        // 定义正则表达式："你好呀  你.+好呀  "
+        matchString = "你好。你好。你好。"
+        
+        
+        XCTAssertTrue(translatedString == matchString, "翻译失败")
+        
+        windowsQuery.buttons["Close"].click()
+        
+        
+        showAppWindows(app: targetApp)
     }
     
     @MainActor
